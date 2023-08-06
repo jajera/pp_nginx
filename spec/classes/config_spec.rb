876d6257469efa92b63be565e4d41b8f04611b28
John Ajera <jdcajera@gmail.com>
2023-08-07 08:47:21 +1200

    initial commit
    initial code commit

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -6,30 +6,19 @@ describe 'nginx::config' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      # let(:pre_condition) { 'include nginx::install' }
-      # let(:pre_condition) { 'include nginx::service' }
-
-      it { is_expected.to compile.with_all_deps }
-      # is_expected.to contain_class('nginx::service')
+      let(:pre_condition) { '
+        class { "nginx::install": }
+        class { "nginx::service": }
+      ' }
+      it 'creates the file /var/www/html/index.html' do
+        is_expected.to compile.with_all_deps
+        is_expected.to contain_class('nginx::install')
+        is_expected.to contain_class('nginx::service')
+        is_expected.to contain_file('/var/www/html/index.html').with(
+          ensure: 'file',
+          content: 'Hello, World!'
+        )
+      end
     end
   end
 end
-
-
-
-# describe 'nginx::install' do
-#   on_supported_os.each do |os, os_facts|
-#     context "on #{os}" do
-#       let(:facts) { os_facts }
-#       let(:pre_condition) { 'include nginx::service' }
-
-#       it 'installs nginx package' do
-#         is_expected.to compile.with_all_deps
-#         is_expected.to contain_class('nginx::service')
-#         is_expected.to contain_package('nginx').with(
-#           ensure: 'installed'
-#        )
-#       end
-#     end
-#   end
-# end
